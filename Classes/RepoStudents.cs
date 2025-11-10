cae915a69964d98f4f31df9023695cd7b706d313
student-a502 <student-a502@STUD-RM20-A502>
2025-11-10 15:25:30 +0500

    Разработанный класс RepoStudents. Бартова

--- Classes/RepoStudents.cs
+++ Classes/RepoStudents.cs
@@ -8,6 +8,26 @@ namespace Constructors_Bartova.Classes
 {
     public class RepoStudents
     {
-       
+        public static List<Student> AllStudents()
+        {
+            List<Student> allStudent = new List<Student>();
+            allStudent.Add(new Student("Бартова", "Елизавета", "Юрьевна", false, 3));
+            allStudent.Add(new Student("Шляпникова", "Юлия", "Сергеевна"));
+            allStudent.Add(new Student("Братчиков", "Максим", "Афанасьевич", false, 2));
+            allStudent.Add(new Student("Бтова", "Елизавета", "Юрьевна", false, 3));
+            allStudent.Add(new Student("Тартова", "Елизавета", "Александровна", true));
+            allStudent.Add(new Student("Шартова", "Алина", "Афанасьевна", false, 3));
+            allStudent.Add(new Student("Мыльников", "Пётр", "Николаевич", false, 3));
+            allStudent.Add(new Student("Сартова", "Мария", "Сергеевна", false, 1));
+            allStudent.Add(new Student("Батова", "Елизавета", "Юрьевна"));
+            allStudent.Add(new Student("Бартова", "Елена", "Сергеевна", false, 3));
+            allStudent.Add(new Student("Бартова", "Елизавета", "Александровна", true, 3));
+            allStudent.Add(new Student("Бартова", "Екатерина", "Сергеевна", true, 3));
+            allStudent.Add(new Student("Бартова", "Елизавета", "Афанасьевна", false, 3));
+            allStudent.Add(new Student("Бартова", "Изольда", "Сергеевна", true, 3));
+            allStudent.Add(new Student("Бартова", "Елизавета", "Юрьевна", false, 3));
+            allStudent.Add(new Student("Бартова", "Мария", "Сергеевна", true, 3));
+            return allStudent;
+        }
     }
 }
